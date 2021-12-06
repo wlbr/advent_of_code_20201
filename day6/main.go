d0d302a61bcfc541b50961a5bae8e20fa541bdc2
Michael Wolber <mwolber@gmx.de>
2021-12-06 13:54:02 +0100

    No need for a struct

--- day6/main.go
+++ day6/main.go
@@ -9,14 +9,14 @@ func countFish(buckets []*Int) *Int {
 	count := NewInt(0)
 
 	for _, b := range buckets {
-		count = count.Add(count, b)
+		count.Add(count, b)
 	}
 	return count
 }
 
 func age(buckets []*Int) []*Int {
 	newbuckets := append(buckets[1:], buckets[0])
-	newbuckets[6] = newbuckets[6].Add(newbuckets[6], newbuckets[len(newbuckets)-1])
+	newbuckets[6].Add(newbuckets[6], newbuckets[len(newbuckets)-1])
 	return newbuckets
 }
 
@@ -26,7 +26,7 @@ func task1and2(fishes []int, days int) (result *Int) {
 		buckets[a] = NewInt(0)
 	}
 	for _, f := range fishes {
-		buckets[f] = buckets[f].Add(buckets[f], NewInt(1))
+		buckets[f].Add(buckets[f], NewInt(1))
 	}
 	for day := 0; day < days; day++ {
 		buckets = age(buckets)
@@ -38,8 +38,10 @@ func main() {
 	input := "input.txt"
 
 	fishes := readdata(input)
+
 	days := 80
 	fmt.Printf("Task 1 - # fishes after %d days   = %d \n", days, task1and2(fishes, days))
 	days = 256
 	fmt.Printf("Task 2 - # fishes after %d days  = %d \n", days, task1and2(fishes, days))
+
 }
